555498abf094f4023debf38c799e52f206530091
James <kendrickj5@yahoo.com>
2025-10-18 18:57:02 -0500

    Update schedule generation for 2025-2026 season, including new file paths and adjustments for all-star break and JSON errors. Remove outdated schedule files.

--- lineup-generation/v1/setup/clean_schedule.py
+++ lineup-generation/v1/setup/clean_schedule.py
@@ -11,52 +11,45 @@ def load_json_file(file_path):
 def convert_time(time_str, from_format="%Y-%m-%dT%H:%M:%SZ", to_format="%m/%d/%Y") -> datetime:
 	return datetime.strptime(   (datetime.strptime(time_str, from_format).strftime(to_format))  , to_format   )
 
-json_file_path = "/Users/jameskendrick/Code/cv/features/lineup-generation/v1/static/schedule_raw.json"
+json_file_path = "/Users/jameskendrick/Code/Projects/cv/features/lineup-generation/v1/static/schedule_raw2025-2026.json"
 data = load_json_file(json_file_path)
 
 schedule = {}
 
 weeks = data["leagueSchedule"]["weeks"]
-for i in range(len(weeks)):
-	week = weeks[i]
-	cur_week = week["weekNumber"]
-	if cur_week == 23 or cur_week == 25:
-		continue
+temp = []
+for i, week in enumerate(weeks):
+	week_number = week["weekNumber"]
 	start_date = convert_time(week["startDate"])
-	end_date = convert_time(week["endDate"])
+	end_date = convert_time(week["endDate"]) if week_number != 17 else convert_time(weeks[i + 1]["endDate"])
+	game_span = (end_date - start_date).days + 1
+	temp.append((week_number, start_date, end_date, game_span))
 
-	# Adjust for all-star break
-	if cur_week == 18:
-		schedule[17]["endDate"] = end_date
-		schedule[17]["gameSpan"] = (end_date - schedule[17]["startDate"]).days
-	elif cur_week > 18:
-		if cur_week <= 21:
-			schedule[cur_week - 1] = {"startDate": start_date,
-										"endDate": end_date,
-										"gameSpan": (end_date - start_date).days}
-		else: # Adjust for playoffs
-			# cur_week = 22 or 24
-			cur_week = 23 if cur_week == 24 else cur_week
-			cur_week -= 1
-			end_date = convert_time(weeks[i + 1]["endDate"])
-			schedule[cur_week] = {"startDate": start_date,
-										"endDate": end_date,
-										"gameSpan": (end_date - start_date).days}
-	else:
-		schedule[cur_week] = {"startDate": start_date,
-							  "endDate": end_date,
-							  "gameSpan": (end_date - start_date).days}
-		
-print(schedule)
+# Adjust for all-star break
+temp.pop(17)
+temp.sort(key=lambda x: x[0])
+for i in range(len(temp)):
+	if i > 17:
+		temp[i] = (temp[i][0] - 1, temp[i][1], temp[i][2], temp[i][3])
 
-season_start = datetime.strptime("10/22/2024", "%m/%d/%Y")
+# Adjust for error in JSON file
+temp.pop(8)
+temp = temp[:20]
+
+for week in temp:
+	schedule[week[0]] = {"startDate": week[1], "endDate": week[2], "gameSpan": week[3]}
+
+# for week_number, info in schedule.items():
+# 	print(week_number, info)
+
+season_start = datetime.strptime("10/21/2025", "%m/%d/%Y")
 game_dates = data["leagueSchedule"]["gameDates"]
 cur_week = 1
 game_date_format = "%m/%d/%Y %H:%M:%S"
 
 games_in_week = defaultdict(dict)
 for day in game_dates:
-	if cur_week >= 23:
+	if cur_week >= 20:
 		break
 	game_date = convert_time(day["gameDate"], game_date_format)
 	week_start_date = schedule[cur_week]["startDate"]
@@ -64,7 +57,7 @@ for day in game_dates:
 	if game_date < season_start:
 		continue
 	if game_date > week_end_date:
-		week_start_date = schedule[cur_week]["startDate"]
+		week_start_date = schedule[cur_week + 1]["startDate"]
 		schedule[cur_week]["games"] = games_in_week
 		cur_week += 1
 		games_in_week = defaultdict(dict)
@@ -81,5 +74,5 @@ for week in schedule:
 	schedule[week]["startDate"] = schedule[week]["startDate"].strftime("%m/%d/%Y")
 	schedule[week]["endDate"] = schedule[week]["endDate"].strftime("%m/%d/%Y")
 
-with open("/Users/jameskendrick/Code/cv/features/lineup-generation/v2/static/schedule24-25.json", 'w') as f:
+with open("/Users/jameskendrick/Code/Projects/cv/features/lineup-generation/v1/static/schedule2025-2026.json", 'w') as f:
 	json.dump(schedule, f, indent=4)
